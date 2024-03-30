5a55c605e075420f09e8f1321ffa55c80215ac60
Michael Martinez <66656582+lonelydomino@users.noreply.github.com>
2024-03-30 19:22:19 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+score = 75
+scoreOfA = 90
+scoreOfB = 80
+scoreOfC = 70
+scoreOfD = 60
+
+if score >= scoreOfA:
+  print('Your grade is A.')
+else:
+  if score >= scoreOfB:
+    print('Your grade is B.')
+
+  else:
+    if score >= scoreOfC:
+      print('Your grade is C.')
+    else:
+      if score >= scoreOfD:
+        print('Your grade is D.')
+      else:
+        print('Your grade is F.')
